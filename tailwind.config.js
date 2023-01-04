1dda93596e23bd5cbc6bd1a6baa7aa0dd2487739
simao234430 <simao234430@gmail.com>
2023-01-04 09:14:23 +0800

    add new project page

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.tsx',
+    './components/**/*.tsx',
+    './layouts/**/*.tsx',
+    './lib/**/*.ts',
+    './data/**/*.mdx',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
