4a075045fbe7ab6f1bc2736ddb9edb36b0108e35
simao234430 <simao234430@gmail.com>
2023-01-11 16:07:12 +0800

    add export pdf use puppeteer

---
+++ pages/api/test.ts
@@ -0,0 +1,38 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import puppeteer from 'puppeteer';
+
+
+const saveAsPdf = async (url: string) => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.goto(url, {
+      waitUntil: 'networkidle0'
+    });
+
+  const result = await page.pdf({
+    format: 'a4',
+  });
+  await browser.close();
+
+  return result;
+};
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse 
+  ) {
+    const { url } = req.query; // pass the page to create PDF from as param
+
+    res.setHeader(
+      'Content-Disposition',
+      `attachment; filename="file.pdf"`
+    );
+    res.setHeader('Content-Type', 'application/pdf');
+  
+    const pdf = await saveAsPdf('http://localhost:3000/blog/why-does-react-need-keys');
+    return res.send(pdf);
+  }
+  
+
+ 
